531e672d32d9be317fba9d7ec7819dc190faad06
Nischal Samji <nischalsamji@gmail.com>
2016-01-31 19:35:27 -0700

    Init Commit for a loooooong ride

---
+++ src/LinkedListOps.java
@@ -0,0 +1,32 @@
+
+public class LinkedListOps {
+	
+	private Node head = null;
+	class Node{
+		private int data;
+		private Node next;
+		
+		Node(int data){
+			this.data = data;
+			this.next = next;
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		
+		
+		
+			
+		
+	}
+	
+	public boolean initLinkedList(int data){
+		
+		Node curr = new Node(data);
+		head.data = (Integer) null;
+		head.next =   curr;
+		return true;
+	}
+
+}
---
+++ src/PrimeComplexity.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class PrimeComplexity {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		System.out.println("Enter a number");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		Long testPrime =  Long.parseLong(br.readLine());
+		
+		System.out.println("Testing slow prime");
+		long st1 = System.nanoTime();
+		boolean val = testPrimeSlow(testPrime);
+		long et1 = System.nanoTime();
+		System.out.println("Time Taken:" + (et1 - st1));
+		System.out.println("Testing fast prime");
+		long st2 = System.nanoTime();
+		boolean val2 = testPrimeFast(testPrime);
+		long et2 = System.nanoTime();
+		System.out.println("Time Taken:" + (et2 - st2));
+		if(val && val2)
+			System.out.println("This is prime");
+		else
+			System.out.println("This is not a prime");
+	}
+	
+	static boolean testPrimeSlow(long tester){
+		for(long i=2 ; i< tester-1; i++){
+			if (tester % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+	static boolean testPrimeFast(long tester){
+		for(long i=2 ; i< Math.sqrt(tester); i++){
+			if (tester % i == 0)
+				return false;
+		}
+		return true;
+	}
+	
+}
